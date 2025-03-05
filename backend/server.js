f767185da6d268b1f0049d052061e114b2a3867b
MohammedKaif4090 <guchiyakaif@gmail.com>
2025-03-05 21:53:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Logging middleware for debugging requests
+// Logging middleware for debugging request
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
