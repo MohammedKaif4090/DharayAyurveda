db60f518bf77beceac835315513919125a165d84
MohammedKaif4090 <guchiyakaif@gmail.com>
2025-03-05 21:17:56 +0530

    Update Cart.jsx

--- src/Components/Context/Cart.jsx
+++ src/Components/Context/Cart.jsx
@@ -48,7 +48,7 @@ const Cart = () => {
     console.log("Submitting order data:", orderData);
 
     try {
-      const response = await fetch("http://localhost:5000/api/orders", {
+      const response = await fetch("https://dharayayurvedas.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
